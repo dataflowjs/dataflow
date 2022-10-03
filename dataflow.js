2d8fa1da1a6ea3845340b5861f9bebfb06fce7ea
dataflowjs <geflowd@gmail.com>
2022-10-03 11:14:00 +0200

    some changes

--- dataflow.js
+++ dataflow.js
@@ -15,6 +15,31 @@
 		return a;
 	}
 
+	function objectsEqual(obj1, obj2) {
+		let keys1 = Object.keys(obj1);
+		let keys2 = Object.keys(obj2);
+
+		if (keys1.length === keys2.length) {
+			return keys1.every(key => obj2.hasOwnProperty(key) && obj2[key] === obj1[key]);
+		}
+
+		return false;
+	}
+
+	function objectKeysEqual(obj1, obj2) {
+		for (let k in obj1) {
+			if (obj2[k] === undefined) {
+				return false;
+			}
+
+			if (obj1[k] !== obj2[k]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
 	const isNumeric = (num) => (typeof (num) === 'number' || typeof (num) === 'string' && num.trim() !== '') && !isNaN(num);
 	const isBool = (val) => {
 		if (typeof (val) != 'string') {
@@ -29,18 +54,30 @@
 			path = path.split('.');
 		}
 
+		if (ctx === undefined) {
+			return path;
+		}
+
+		let ctxCount = 0;
 		for (let i = 0; i < path.length; i++) {
 			let p = path[i];
 			if (typeof p === 'string' && p.charAt(0) === '$') {
-				if (ctx === undefined || ctx[p] === undefined) {
-					return path;
+				let key;
+				if (Array.isArray(ctx)) {
+					key = ctx[ctxCount];
+					ctxCount++;
+				} else {
+					key = ctx[p];
 				}
-				path[i] = ctx[p];
-			}
-		}
 
-		if (ctx !== undefined) {
-			ctx.prepared = true;
+				if (key === undefined && ctx.el !== undefined) {
+					key = ctx.el.dataflow.context[p];
+				}
+
+				if (key === undefined) return;
+
+				path[i] = key;
+			}
 		}
 
 		return path;
@@ -50,7 +87,7 @@
 		for (let i = 0; i < watchers.length; i++) {
 			let w = watchers[i];
 			if (w.args === undefined && w.path !== undefined) {
-				w.fn(df.get(w.path, w.ctx));
+				w.fn(df.get(w.path, w));
 			} else {
 				let args = df.prepare(w);
 				if (w.fn !== undefined) {
@@ -157,12 +194,11 @@
 					};
 					continue;
 
-				case c === '~' && !isString && dynArg !== null:
-					dynArg.flow = 'g';
-					continue;
-
 				case c === '.' && !isString && dynArg !== null:
-					if (tmp === '') continue;
+					if (tmp === '') {
+						dynArg.flow = 'g';
+						continue;
+					}
 					dynArg.path.push(tmp);
 
 					tmp = '';
@@ -292,7 +328,7 @@
 		},
 
 		watcher: function (path, watcher) {
-			path = pathPrepare(path, watcher.ctx);
+			path = pathPrepare(path, watcher);
 
 			if (path === undefined) {
 				return;
@@ -325,16 +361,12 @@
 					tmp[path[i]].watchers.push(watcher);
 
 					if (watcher.el !== undefined) {
-						if (watcher.el.dataflow === undefined) {
-							watcher.el.dataflow = {};
-							if (watcher.el.dataflow.watchers === undefined) {
-								watcher.el.dataflow.watchers = [{
-									watcherId: watcher.watcherId,
-									path: path
-								}];
-							};
+						this.setup(watcher.el);
 
-						}
+						watcher.el.dataflow.watchers.push({
+							watcherId: watcher.watcherId,
+							path: path
+						});
 					}
 					return;
 				}
@@ -344,18 +376,15 @@
 		},
 
 		flow: function (path, ctx) {
-			// console.time('flow');
-			if (ctx !== undefined && ctx.flow === 'none') {
+			if (ctx === undefined) {
+				ctx = {};
+			}
+
+			if (ctx.flow === 'none') {
 				return;
 			}
 
-			if (ctx !== undefined && ctx.prepared !== true) {
-				path = pathPrepare(path, ctx);
-				if (path === undefined) {
-					return;
-				}
-				ctx.prepared = false;
-			}
+			path = pathPrepare(path, ctx);
 
 			let len = path.length;
 			let tmp = $$watchers;
@@ -365,11 +394,11 @@
 						return;
 					}
 
-					if (ctx !== undefined && (ctx.flow === 'before' || ctx.flow === 'all')) {
+					if (ctx.flow === 'before' || ctx.flow === 'all') {
 						flow(tmp[path[i]].watchers);
 					}
 
-					if (ctx !== undefined && ctx.flow === 'deep') {
+					if (ctx.flow === 'deep') {
 						flowDeep(tmp[path[i]].keys);
 						return;
 					}
@@ -380,13 +409,12 @@
 					if (tmp[path[i]] !== undefined) {
 						flow(tmp[path[i]].watchers);
 
-						if (ctx !== undefined && (ctx.flow === 'after' || ctx.flow === 'all')) {
+						if (ctx.flow === 'after' || ctx.flow === 'all') {
 							flowDeep(tmp[path[i]].keys);
 						}
 					}
 				}
 			}
-			// console.timeEnd('flow');
 		},
 
 		inject: function (comp, stg) {
@@ -426,6 +454,7 @@
 
 			fn.call(ctx, stg);
 
+			ctx.compCtx = ctx;
 			this.set(comp, ctx);
 
 			if (stg.el !== undefined) {
@@ -436,7 +465,7 @@
 
 			this.set(comp + '.init', true);
 
-			ctx.el.innerHTML = ctx.template;
+			ctx.el.insertAdjacentHTML('beforeend', ctx.template);
 
 			this.exec(ctx);
 			this.set(comp + '.ready', true);
@@ -446,6 +475,8 @@
 			if (ctx.el === undefined) {
 				ctx.el = document.querySelector(ctx.root);
 			}
+
+			this.setup(ctx.el);
 
 			let els = [];
 
@@ -462,8 +493,15 @@
 			len = els.length;
 			for (let i = 0; i < len; i++) {
 				let node = els[i];
-				if (node.dataflow !== undefined && node.dataflow.omit) {
+
+				this.setup(node);
+
+				if (node.dataflow.omit) {
 					continue;
+				}
+
+				if (node.dataflow.once) {
+					node.dataflow.omit = true;
 				}
 
 				let attrs = node.attributes;
@@ -478,34 +516,39 @@
 						ctx: ctx
 					};
 
+					this.transfer(ctx, watcher);
+
 					switch (an.charAt(0)) {
 						case '@':
+							watcher.args = this.parse(av);
 
-							watcher.args = df.parse(av);
-							df.register(watcher);
+							this.register(watcher);
 
-							node.addEventListener(an.slice(1), function () {
+							let eventName = an.slice(1);
+							let eventFn = function () {
 								df.prepare(watcher);
+							};
+
+							node.addEventListener(eventName, eventFn);
+
+							watcher.el.dataflow.events.push({
+								eventName,
+								eventFn
 							});
 
 							break;
 
 						case ':':
-							if ($$directives[an] === undefined) {
-								return;
-							}
-
 							// watcher.args = argsCache(av);
-							watcher.args = df.parse(av);
+							watcher.args = this.parse(av);
 
-							df.register(watcher);
+							this.register(watcher);
 
-							if (watcher.register === true) {
-								watcher.register = false;
+							if (ctx.register || node.dataflow.register) {
 								continue;
 							}
 
-							watcher.fn.apply(watcher, df.prepare(watcher));
+							watcher.fn.apply(watcher, this.prepare(watcher));
 
 							break;
 
@@ -517,6 +560,14 @@
 
 		set: function (path, val, ctx) {
 			let self = this;
+
+			if (typeof ctx !== 'object') {
+				ctx = [];
+				for (let i = 2; i < arguments.length; i++) {
+					ctx.push(arguments[i]);
+				}
+			}
+
 			path = pathPrepare(path, ctx);
 
 			if (path === undefined) {
@@ -546,8 +597,14 @@
 				return;
 			}
 
-			let len = path.length;
+			if (typeof ctx !== 'object') {
+				ctx = [];
+				for (let i = 2; i < arguments.length; i++) {
+					ctx.push(arguments[i]);
+				}
+			}
 
+			let len = path.length;
 			let tmp = $$data;
 			for (let i = 0; i < len; i++) {
 				if (i < len - 1) {
@@ -561,9 +618,19 @@
 			}
 		},
 
-		unset: function (path) {
+		unset: function (path, ctx) {
+			let self = this;
+			path = pathPrepare(path, ctx);
+
 			if (path === undefined) {
 				return;
+			}
+
+			if (typeof ctx !== 'object') {
+				ctx = [];
+				for (let i = 2; i < arguments.length; i++) {
+					ctx.push(arguments[i]);
+				}
 			}
 
 			let len = path.length;
@@ -576,6 +643,8 @@
 					tmp = tmp[path[i]];
 				} else {
 					delete tmp[path[i]];
+
+					self.flow(path, ctx);
 				}
 			}
 		},
@@ -585,60 +654,86 @@
 				el = document.querySelector(el);
 			}
 
-			if (el.dataflow === undefined || el.dataflow.watchers === undefined) {
-				return;
+			let els = [];
+			if (ctx !== undefined && ctx.container) {
+				els = [el];
 			}
 
-			let unset = true;
-			if (ctx !== undefined && ctx.data === false) {
-				unset = false;
-			}
-
-			for (let i = 0; i < el.dataflow.watchers.length; i++) {
-				let w = el.dataflow.watchers[i];
-				let len = w.path.length;
-				let tmp = $$watchers;
+			if (ctx !== undefined && ctx.deep !== undefined) {
+				let nodes = el.querySelectorAll('*');
+				let len = nodes.length;
 				for (let i = 0; i < len; i++) {
-					if (tmp[w.path[i]].len === 1) {
-						delete tmp[w.path[i]];
-						if (unset) {
-							this.unset(w.path.slice(0, i + 1));
-						}
-						break;
-					}
-
-					tmp[w.path[i]].len--;
-
-					if (i === len - 1) {
-						for (let i1 = 0; i1 < tmp[w.path[i]].watchers.length; i1++) {
-							let w1 = tmp[w.path[i]].watchers[i1];
-							if (w.watcherId === w1.watcherId) {
-								tmp[w.path[i]].watchers.splice(i1, 1);
-							}
-						}
-						break;
-					}
-
-					tmp = tmp[w.path[i]].keys;
+					els.push(nodes[i]);
 				}
+			}
+
+			let unset = false;
+			if (ctx !== undefined && ctx.data) {
+				unset = true;
+			}
+
+			for (let i = 0; i < els.length; i++) {
+				el = els[i];
+
+				if (el.dataflow === undefined) {
+					continue;
+				}
+
+				if (el.dataflow.events !== undefined) {
+					for (let i1 = 0; i1 < el.dataflow.events.length; i1++) {
+						let e = el.dataflow.events[i1];
+						el.removeEventListener(e.eventName, e.eventFn);
+					}
+
+					el.dataflow.events = [];
+				}
+
+				if (el.dataflow.watchers === undefined) {
+					continue;
+				}
+
+				for (let i1 = 0; i1 < el.dataflow.watchers.length; i1++) {
+					let w = el.dataflow.watchers[i1];
+					let len = w.path.length;
+					let tmp = $$watchers;
+					for (let i2 = 0; i2 < len; i2++) {
+						if (tmp[w.path[i2]].len === 1) {
+							delete tmp[w.path[i2]];
+							if (unset) {
+								this.unset(w.path.slice(0, i2 + 1));
+							}
+							break;
+						}
+
+						tmp[w.path[i2]].len--;
+						if (i2 === len - 1) {
+							for (let i3 = 0; i3 < tmp[w.path[i2]].watchers.length; i3++) {
+								let w1 = tmp[w.path[i2]].watchers[i3];
+								if (w.watcherId === w1.watcherId) {
+									tmp[w.path[i2]].watchers.splice(i3, 1);
+								}
+							}
+							continue;
+						}
+
+						tmp = tmp[w.path[i2]].keys;
+					}
+				}
+
+				el.dataflow.watchers = [];
 			}
 		},
 
 		remove: function (el, ctx) {
-			let self = this;
-
 			if (typeof el === 'string') {
 				el = document.querySelector(el);
 			}
 
-			if (ctx !== undefined && ctx.container) {
-				self.clean(el, ctx);
-			}
+			this.clean(el, ctx);
 
-			el.querySelectorAll('*').forEach(function (node) {
-				self.clean(node, ctx);
-				node.parentNode.removeChild(node);
-			});
+			while (el.firstChild) {
+				el.removeChild(el.lastChild);
+			}
 		},
 
 		parse: function (str) {
@@ -658,10 +753,10 @@
 			for (let i = 0; i < watcher.args.watchers.length; i++) {
 				let w = watcher.args.watchers[i];
 
-				if ((w.flow === 'l' || w.flow === 'p') && watcher.ctx.comp !== undefined) {
-					let len = watcher.ctx.comp.length;
+				if ((w.flow === 'l' || w.flow === 'p') && watcher.compCtx !== undefined) {
+					let len = watcher.compCtx.comp.length;
 					while (len--) {
-						w.path.unshift(watcher.ctx.comp[len]);
+						w.path.unshift(watcher.compCtx.comp[len]);
 					}
 				}
 
@@ -701,16 +796,16 @@
 						break;
 
 					default:
-						s.obj[s.pos] = watcher.ctx[s.name];
+						s.obj[s.pos] = watcher.ctx[s.name] !== undefined ? watcher.ctx[s.name] : watcher.el.dataflow.keys[s.name];
 				}
 			}
 
 			for (let i = 0; i < watcher.args.watchers.length; i++) {
 				let o = watcher.args.watchers[i];
 				if (o.flow !== 'p') {
-					o.obj[o.pos] = df.get(o.path, watcher.ctx);
+					o.obj[o.pos] = df.get(o.path, watcher);
 				} else {
-					o.obj[o.pos] = pathPrepare(o.path, watcher.ctx);
+					o.obj[o.pos] = o.path;
 				}
 			}
 
@@ -730,18 +825,55 @@
 			return watcher.args.obj;
 		},
 
-		transfer: function (to, from) {
-			if (from.ctx.comp !== undefined) {
-				to.comp = from.ctx.comp;
-				to.funcs = from.ctx.funcs;
-				to.set = from.ctx.set;
-				to.get = from.ctx.get;
+		setup: function (el) {
+			if (el.dataflow === undefined) {
+				el.dataflow = {};
 			}
+
+			if (el.dataflow.watchers === undefined) {
+				el.dataflow.watchers = [];
+			}
+
+			if (el.dataflow.context === undefined) {
+				el.dataflow.context = {};
+			}
+
+			if (el.dataflow.events === undefined) {
+				el.dataflow.events = [];
+			}
+
+			if (el.dataflow.directives === undefined) {
+				el.dataflow.directives = {};
+			}
+		},
+
+		transfer: function (from, to) {
+			if (to === undefined) {
+				to = {};
+			}
+
+			if (from.compCtx !== undefined) {
+				to.compCtx = from.compCtx;
+			}
+
+			for (let k in from.el.dataflow.context) {
+				to.el.dataflow.context[k] = from.el.dataflow.context[k];
+			}
+
+			return to;
+		},
+
+		assign: function () {
+			return Object.assign({}, df.get(arrayFrom(arguments)));
 		}
 	};
 
-	df.func('set', function (path, val, ctx) {
-		df.set(path, val, ctx);
+	df.func('set', function () {
+		let args = arrayFrom(arguments);
+		if (args.length == 2) {
+			args.push(this);
+		}
+		df.set.apply(df, args);
 	});
 
 	df.func('stringify', function (val) {
@@ -822,10 +954,46 @@
 		}
 	});
 
+	df.func('classTrue', function () {
+		let args = arrayFrom(arguments);
+		if (args[0]) {
+			for (let i = 1; i < args.length; i++) {
+				this.el.classList.add(args[i]);
+			}
+			return;
+		}
+
+		for (let i = 1; i < args.length; i++) {
+			this.el.classList.remove(args[i]);
+		}
+	});
+
+	df.func('classFalse', function () {
+		let args = arrayFrom(arguments);
+		args[0] = !args[0];
+		df.func('classTrue').apply(this, args);
+	});
+
 	df.directive('noop', function () { });
 
 	df.directive('text', function (val, defVal) {
 		this.el.textContent = val || defVal;
+	});
+
+	df.directive('attr', function (access, name, value) {
+		if (access) {
+			this.el.setAttribute(name, value);
+		}
+	});
+
+	df.directive('rowspan', function (access, value) {
+		if (access) {
+			this.el.setAttribute('rowspan', value);
+			return;
+		}
+
+		df.remove(this.el, { container: true, deep: true });
+		this.el.parentElement.removeChild(this.el);
 	});
 
 	df.directive('component', function (name, params) {
@@ -833,59 +1001,78 @@
 			params = {};
 		}
 		params.el = this.el;
+		df.transfer(this, params);
 		df.inject(name, params);
 	});
 
-	df.directive('repeat', function (val, stg) {
-		// console.time('repeat');
-		if (this.el.dataflow === undefined) {
-			this.el.dataflow = {};
+	df.directive('range', function (val, stg) {
+		if (typeof stg === 'string') {
+			stg = {
+				key: stg
+			};
+		}
+		// console.time('range');
+		let directives = this.el.dataflow.directives;
+		if (directives.range === undefined) {
+			directives.range = {};
 		}
 
-		if (this.el.dataflow.repeat === undefined) {
-			this.el.dataflow.repeat = {};
-		}
+		let range = directives.range;
 
-		let repeat = this.el.dataflow.repeat;
-
-		if (repeat.el === undefined) {
+		if (range.el === undefined) {
 			let firstChild = this.el.firstElementChild
-			repeat.el = firstChild.cloneNode(true);
+			range.el = firstChild.cloneNode(true);
 
-			let nested = this.el.querySelectorAll('*');
-			for (let i = 0; i < nested.length; i++) {
-				let n = nested[i];
-				if (n.dataflow === undefined) {
-					n.dataflow = {};
-				}
-				n.dataflow.omit = true;
+			let nodes = this.el.querySelectorAll('*');
+			for (let i = 0; i < nodes.length; i++) {
+				let node = nodes[i];
+				df.setup(node);
+				node.dataflow.omit = true;
 			}
 
 			this.el.removeChild(firstChild);
 
-			repeat.el.classList.remove('df-hide');
+			range.el.classList.remove('df-hide');
 		}
 
 		let dataRemove = false;
-		if (stg !== undefined && stg.data) {
+		if (stg !== undefined && stg.data !== undefined) {
 			dataRemove = true;
 		}
 
-		df.remove(this.el, { data: dataRemove });
+		let keyName;
+		if (stg !== undefined && stg.key !== undefined) {
+			keyName = stg.key;
+		}
 
-		if (repeat.fn === undefined) {
-			repeat.fn = function (self, key, val) {
+		df.remove(this.el, { data: dataRemove, deep: true });
+
+		if (range.fn === undefined) {
+			range.fn = function (self, key, val) {
+				let clone = range.el.cloneNode(true);
+				df.setup(clone);
+
 				let ctx = {};
-				if (self.ctx.comp !== undefined) {
-					ctx.comp = self.ctx.comp;
+				ctx.el = clone;
+
+				self.el.dataflow.context[key] = val;
+
+				let nodes = ctx.el.querySelectorAll('*');
+				for (let i = 0; i < nodes.length; i++) {
+					let node = nodes[i];
+					df.setup(node);
+
+					for (let k in self.el.dataflow.context) {
+						node.dataflow.context[k] = self.el.dataflow.context[k];
+					}
 				}
 
-				let clone = repeat.el.cloneNode(true);
-				ctx.el = clone;
-				ctx[key] = val;
 				ctx.container = true;
-				df.transfer(ctx, self);
+
+				df.transfer(self, ctx);
+
 				df.exec(ctx);
+
 				self.el.insertAdjacentElement('beforeend', clone);
 			}
 		}
@@ -893,18 +1080,18 @@
 		switch (true) {
 			case Array.isArray(val):
 				for (let i = 0; i < val.length; i++) {
-					repeat.fn(this, '$index', i);
+					range.fn(this, keyName || '$index', i);
 				}
 				break;
 
 			case typeof val === 'object':
 				for (let k in val) {
-					repeat.fn(this, '$key', k);
+					range.fn(this, keyName || '$key', k);
 				}
 				break;
 		}
 
-		// console.timeEnd('repeat');
+		// console.timeEnd('range');
 	});
 
 	df.directive('hide', function (val, dir) {
@@ -939,9 +1126,79 @@
 	});
 
 	df.directive('class', df.func('class'));
+	df.directive('class.true', df.func('classTrue'));
+	df.directive('class.false', df.func('classFalse'));
 
 	df.directive('if', df.func('if'));
 
+	df.directive('context', function (keys) {
+		let directives = this.el.dataflow.directives;
+		if (directives.context === undefined) {
+			directives.context = {};
+		}
+
+		let context = directives.context;
+
+		if (!context.init) {
+			context.init = true;
+			context.once = false;
+
+			for (let k in keys) {
+				this.el.dataflow.context[k] = keys[k];
+			}
+
+			let nodes = this.el.querySelectorAll('*');
+			for (let i = 0; i < nodes.length; i++) {
+				let node = nodes[i];
+
+				for (let k in keys) {
+					node.dataflow.context[k] = keys[k];
+				}
+			}
+			return;
+		}
+
+		if (context.once) {
+			context.once = false;
+			return;
+		}
+		context.once = true;
+
+		if (objectKeysEqual(keys, this.el.dataflow.context)) {
+			context.once = false;
+			return;
+		}
+
+		for (let k in keys) {
+			this.el.dataflow.context[k] = keys[k];
+		}
+
+		let nodes = this.el.querySelectorAll('*');
+
+		for (let i = 0; i < nodes.length; i++) {
+			let node = nodes[i];
+
+			for (let k in keys) {
+				node.dataflow.context[k] = keys[k];
+			}
+		}
+
+		df.clean(this.el, {
+			container: true,
+			deep: true
+		});
+
+		let ctx = df.transfer(this, { el: this.el });
+		ctx.container = true;
+
+		df.exec(ctx);
+
+		context.once = false;
+	});
+
+	df.directive('log', function () {
+		console.log(arguments);
+	});
 
 	document.addEventListener("DOMContentLoaded", function () {
 		df.set('dataflow.ready', true);
